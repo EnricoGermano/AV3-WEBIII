46008bb6b25cc9a5f1a3491505f077d2e8dfa193
Enrico de Chiara Germano <enrico.germano@fatec.sp.gov.br>
2026-05-15 08:12:05 -0300

    feat arrumando path

--- automanager-av3/src/main/java/com/autobots/automanager/controles/GersonControle.java
+++ automanager-av3/src/main/java/com/autobots/automanager/controles/GersonControle.java
@@ -1,9 +1,8 @@
 package com.autobots.automanager.controles;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GersonControle {
 
-    @GetMapping(value = "/gerson", produces = MediaType.IMAGE_JPEG_VALUE)
+    @GetMapping(value = "/gerson", produces = MediaType.IMAGE_PNG_VALUE)
     public byte[] imagem() throws IOException {
-        return Files.readAllBytes(Path.of("src/main/resources/static/gerson.png"));
+        return new ClassPathResource("static/gerson.png").getInputStream().readAllBytes();
     }
 }
